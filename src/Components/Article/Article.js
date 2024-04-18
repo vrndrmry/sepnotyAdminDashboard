3d767d989d4323f30c7de93fbf4b01fe289975c8
Geetam <geetambinani6@gmail.com>
2024-04-18 20:53:04 +0530

    add pagintion feature in response received and created pagination component

--- src/Components/Article/Article.js
+++ src/Components/Article/Article.js
@@ -1,10 +1,103 @@
-import React from 'react'
-import './article.css'
+import React, { useEffect, useState } from "react";
+import "./article.css";
 
 export default function Article() {
+  const [articles, setArticles] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(0);
+
+  const fetchArticlesData = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:8800/article/getArticles?page=${page}`,
+        {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      if (!response.ok) {
+        setArticles([]);
+        return;
+      }
+      if (!total) {
+        setTotal(data.total);
+        setItemsPerPage(data.itemsPerPage);
+      }
+
+      setArticles(data.articles);
+    } catch (error) {
+      setArticles([]);
+    }
+  };
+
+  const calculateSerialNumber = (index) => {
+    return (page - 1) * itemsPerPage + index + 1;
+  };
+  useEffect(() => {
+    fetchArticlesData();
+  }, [page]);
   return (
-    <div className='article'>
-    Article
+    <div className="container">
+      <div className="articleTopSection">
+        <h1>My Articles</h1>
+        <button
+        
+        className="articleBtn">Want to add ??</button>
+      </div>
+      <div className="table-wrapper">
+        <table>
+          <thead>
+            <tr>
+              <th>S.No</th>
+              <th>Title</th>
+              <th>Content</th>
+              <th>Image</th>
+              <th>Author</th>
+              <th>View</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles?.length > 0 &&
+              articles.map((article, index) => (
+                
+                 <>
+                 <tr>
+                  <td >
+                    <span>{calculateSerialNumber(index)}</span>
+                  </td>
+                  <td>{article.title}</td>
+                  <td>{article.content}</td>
+                  <td>{article.imageUrl}</td>
+                  <td>{article.author}</td>
+                  <td>link</td>
+                  <td>action</td>
+                </tr>
+
+                <tr>
+                  <td className="serialNo">
+                    <span>{calculateSerialNumber(index)}</span>
+                  </td>
+                  <td>{article.title}</td>
+                  <td>{article.content}</td>
+                  <td>{article.imageUrl}</td>
+                  <td>{article.author}</td>
+                  <td>link</td>
+                  <td>action</td>
+                </tr>
+                 </>
+                
+               
+              ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
--- src/Components/ResponseReceived/Response.js
+++ src/Components/ResponseReceived/Response.js
@@ -2,16 +2,19 @@ import React, { useContext, useEffect, useState } from "react";
 import "./response.css";
 import { UserContext } from "../../Context/userContext";
 import { useSelector } from "react-redux";
+import Pagination from "../Pagination/Pagination";
 
 export default function Response() {
-  const { userInfo } = useContext(UserContext);
+  // const { userInfo } = useContext(UserContext);
   const [contactUsData, setContactUsData] = useState([]);
   const { currentUser } = useSelector((state) => state.user);
-  const [messageStatus, setMessageStatus] = useState(false)
-
+  const [messageStatus, setMessageStatus] = useState(false);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(0);
   const fetchContactUsData = async () => {
     const response = await fetch(
-      `http://localhost:8800/${currentUser.id}/dashboard/${currentUser.id}/response`,
+      `http://localhost:8800/${currentUser.id}/dashboard/${currentUser.id}/response?page=${page}`,
       {
         method: "GET",
         credentials: "include",
@@ -22,13 +25,27 @@ export default function Response() {
       }
     );
     const data = await response.json();
-    setContactUsData(data);
+
+    setContactUsData(data.recievedData);
+    if (!total) {
+      setTotal(data.total);
+      setItemsPerPage(data.itemsPerPage);
+    }
   };
 
+  const calculateSerialNumber = (index) => {
+    return (page - 1) * itemsPerPage + index + 1;
+  };
   useEffect(() => {
-    fetchContactUsData()
-  }, []);
+    fetchContactUsData();
+  }, [page]);
 
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
+  const handlePreviousPage = () => {
+    setPage(page - 1);
+  };
   return (
     <div className="response">
       <h1>Responsed Recieved</h1>
@@ -48,14 +65,18 @@ export default function Response() {
           <tbody>
             {contactUsData.map((elem, index) => (
               <tr key={elem._id}>
-                <td>{index + 1}</td>
+                <td>{calculateSerialNumber(index)}</td>
                 <td>{elem.username}</td>
                 <td>{elem.company}</td>
                 <td>{elem.email}</td>
                 <td>{elem.phone}</td>
                 <td>{elem.subject}</td>
                 <td>
-                  {!messageStatus && <p onClick={()=>setMessageStatus(!messageStatus)}>See Message</p>}
+                  {!messageStatus && (
+                    <p onClick={() => setMessageStatus(!messageStatus)}>
+                      See Message
+                    </p>
+                  )}
                   {messageStatus && elem.message}
                 </td>
               </tr>
@@ -63,6 +84,13 @@ export default function Response() {
           </tbody>
         </table>
       </div>
+
+      <Pagination
+        page={page}
+        handlePreviousPage={handlePreviousPage}
+        handleNextPage={handleNextPage}
+        total={total}
+      />
     </div>
   );
 }
